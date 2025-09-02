e56cecdbef2385fe87bae9853214dd82aae21942
danilofreitas77 <danilocfreitass77@gmail.com>
2025-09-02 10:14:04 -0300

    adicionando um novo campo a tabela estoque

--- estoque/models.py
+++ estoque/models.py
@@ -4,6 +4,7 @@ class item(models.Model):
     nome = models.CharField(max_length=100)
     preco = models.DecimalField(max_digits=6, decimal_places=2)
     quantidade = models.IntegerField()
+    categoria = models.CharField(max_length=100, default="Outros")
     
     def __str__(self):
         return f"{self.nome} ({self.quantidade} unid.)"
